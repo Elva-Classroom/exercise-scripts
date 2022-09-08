71fd00df2d77b55e6c45e4cc1d0e29256f42a7af
bootswithdefer <bootswithdefer@users.noreply.github.com>
2022-09-07 21:06:39 -0700

    Update setup-labs.sh

--- setup-labs.sh
+++ setup-labs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/environment
 echo "variable \"c9_project\" {
-  value=\"$C9_PROJECT\"
+  default = \"$C9_PROJECT\"
 }" > c9_project.tf
 REPOS="terraform-iam terraform-vpc terraform-eks terraform-data-integration-student terraform-data-integration terraform-rds-mysql terraform-ghost ubuntu-on-kubernetes"
 for REPO in $REPOS; do
