c8a30ca78b501e509cc4e63748ddadd74b1da3c0
bootswithdefer <bootswithdefer@users.noreply.github.com>
2022-09-07 20:44:27 -0700

    Update setup-labs.sh

--- setup-labs.sh
+++ setup-labs.sh
@@ -4,5 +4,7 @@ echo "c9_project=\"$C9_PROJECT\"" > terraform.tfvars
 REPOS="terraform-iam terraform-vpc terraform-eks terraform-data-integration-student terraform-data-integration terraform-rds-mysql terraform-ghost ubuntu-on-kubernetes"
 for REPO in $REPOS; do
   git clone git@github.com:Elva-Classroom/$REPO.git
-  cp terraform.tfvars $REPO/c9_project.auto.tfvars
+  if [ -d $REPO/terraform ]; then
+    cp terraform.tfvars $REPO/terraform/c9_project.auto.tfvars
+  fi
 done
