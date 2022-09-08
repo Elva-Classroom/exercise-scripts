b3a45232d9749984b7d1e7dd13836aeac3bb0f1d
bootswithdefer <bootswithdefer@users.noreply.github.com>
2022-09-07 20:56:30 -0700

    Update cloud9-setup.sh

--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -3,4 +3,4 @@ git clone https://github.com/hashivim/vim-terraform.git ~/.vim/pack/plugins/star
 aws --region us-west-2 secretsmanager get-secret-value --secret-id asu-github-ssh-key | jq -r .SecretString > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 ssh-keyscan github.com > ~/.ssh/known_hosts
-ssh git@github.com
+ssh -T git@github.com
