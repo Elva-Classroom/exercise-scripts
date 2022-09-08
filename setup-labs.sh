14201d77f1bad9fce828caeb6d3aed0cfb215e0c
bootswithdefer <bootswithdefer@users.noreply.github.com>
2022-09-07 20:33:31 -0700

    Update setup-labs.sh

--- setup-labs.sh
+++ setup-labs.sh
@@ -2,3 +2,8 @@ cd ~/environment
 git clone git@github.com:Elva-Classroom/terraform-iam.git
 git clone git@github.com:Elva-Classroom/terraform-vpc.git
 git clone git@github.com:Elva-Classroom/terraform-eks.git
+git clone git@github.com:Elva-Classroom/terraform-data-integration-student.git
+git clone git@github.com:Elva-Classroom/terraform-data-integration.git
+git clone git@github.com:Elva-Classroom/terraform-rds-mysql.git
+git clone git@github.com:Elva-Classroom/terraform-ghost.git
+git clone git@github.com:Elva-Classroom/ubuntu-on-kubernetes.git
