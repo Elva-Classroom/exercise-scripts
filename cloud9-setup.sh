d99a22b04f4018f24abdfcdc259ca35a3689b69d
bootswithdefer <bootswithdefer@users.noreply.github.com>
2022-09-07 20:54:31 -0700

    Update cloud9-setup.sh

--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -2,4 +2,5 @@ sudo yum install jq -y
 git clone https://github.com/hashivim/vim-terraform.git ~/.vim/pack/plugins/start/vim-terraform
 aws --region us-west-2 secretsmanager get-secret-value --secret-id asu-github-ssh-key | jq -r .SecretString > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
+ssh-keyscan github.com > ~/.ssh/known_hosts
 ssh git@github.com
