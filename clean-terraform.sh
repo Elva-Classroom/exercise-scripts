23fb77ba0c1934e99a66be61122c4a2f89ba4c37
EC2 Default User <ec2-user@ip-10-0-102-93.us-west-2.compute.internal>
2022-09-11 23:24:54 +0000

    terraform plugin cache

--- clean-terraform.sh
+++ clean-terraform.sh
@@ -1,2 +1,4 @@
 echo "Cleaning Terraform working directories..."
 rm -rf ~/environment/*/terraform/.terraform
+rm -rf ~/.terraform.d/plugin-cache
+mkdir -p ~/.terraform.d/plugin-cache
--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -3,5 +3,7 @@ git clone https://github.com/hashivim/vim-terraform.git ~/.vim/pack/plugins/star
 aws --region us-west-2 secretsmanager get-secret-value --secret-id asu-github-ssh-key | jq -r .SecretString > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 ssh-keyscan github.com >> ~/.ssh/known_hosts
+mkdir -p ~/.terraform.d/plugin-cache
+echo "plugin_cache_dir = \"$HOME/.terraform.d/plugin-cache\"" > ~/.terraformrc
 echo
 ssh -T git@github.com
