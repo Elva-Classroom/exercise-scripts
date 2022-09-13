1ff1946a09c38b14fd6fa5680436e033ebb72f58
EC2 Default User <ec2-user@ip-10-0-102-93.us-west-2.compute.internal>
2022-09-13 17:39:39 +0000

    move region set to top

--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+aws configure set region us-west-2
+
 sh ./increase-disk.sh
 
 sudo yum install jq gettext bash-completion moreutils -y
@@ -22,7 +25,6 @@ unzip -qo awscliv2.zip
 sudo ./aws/install --update
 rm awscliv2.zip
 rm -r aws
-aws configure set region us-west-2
 
 echo 'export LBC_VERSION="v2.4.1"' >>  ~/.bash_profile
 echo 'export LBC_CHART_VERSION="1.4.1"' >>  ~/.bash_profile
