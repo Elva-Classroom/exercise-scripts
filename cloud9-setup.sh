48fd8f8614b287c053e08a742959fb9af6b0a071
EC2 Default User <ec2-user@ip-10-0-102-93.us-west-2.compute.internal>
2022-09-21 04:30:25 +0000

    update kubectl version

--- cloud9-setup.sh
+++ cloud9-setup.sh
@@ -32,7 +32,7 @@ echo "plugin_cache_dir = \"$HOME/.terraform.d/plugin-cache\"" > ~/.terraformrc
 rm ~/.bash_completion
 
 sudo curl --silent --location -o /usr/local/bin/kubectl \
-	   https://s3.us-west-2.amazonaws.com/amazon-eks/1.21.5/2022-01-21/bin/linux/amd64/kubectl
+	https://s3.us-west-2.amazonaws.com/amazon-eks/1.22.6/2022-03-09/bin/linux/amd64/kubectl
 sudo chmod +x /usr/local/bin/kubectl
 kubectl completion bash >>  ~/.bash_completion
 
